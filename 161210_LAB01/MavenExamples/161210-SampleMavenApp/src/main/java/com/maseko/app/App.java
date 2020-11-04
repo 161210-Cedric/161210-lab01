b313209b532ada45bcb3a286277f609be823ffe6
161210-Cedric <73900943+161210-Cedric@users.noreply.github.com>
2020-11-04 04:33:57 -0800

    Add files via upload

---
+++ 161210_LAB01/MavenExamples/161210-SampleMavenApp/src/main/java/com/maseko/app/App.java
@@ -0,0 +1,13 @@
+package com.maseko.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome - Cedric Siphiwo Maseko " );
+    }
+}
